a272f454b24da1f25d85e32515ea4124c9eed8a1
ptaototo <pesi@tutanota.com>
2021-01-07 12:03:40 -0800

    add project folders

---
+++ Fitness-Application/pa5.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+using std::ifstream;
+using std::ostream;
+using std::istream;
+using std::fstream;
+using std::ofstream;
+
+#define EXERCISE_FILE "exercisePlans.txt"
+#define DIET_FILE "dietPlans.txt"
+
