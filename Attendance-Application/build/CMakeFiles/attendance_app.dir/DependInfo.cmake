9fe43ec555659881b6cda33fea95354fbe02dc52
pe-si <pesi@tutamail.com>
2021-08-10 12:56:39 -1000

    organizing folder structure; use cmake rather than a makefile

---
+++ Attendance-Application/build/CMakeFiles/attendance_app.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/List.cpp" "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/build/CMakeFiles/attendance_app.dir/List.cpp.o"
+  "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/ListNode.cpp" "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/build/CMakeFiles/attendance_app.dir/ListNode.cpp.o"
+  "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/main.cpp" "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/build/CMakeFiles/attendance_app.dir/main.cpp.o"
+  "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/menu.cpp" "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/build/CMakeFiles/attendance_app.dir/menu.cpp.o"
+  "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/pa7.cpp" "/home/pt/Dev/github/pe-si/cs-courses-code/Attendance-Application/build/CMakeFiles/attendance_app.dir/pa7.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
